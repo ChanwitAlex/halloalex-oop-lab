387fb5399c530dc39ee7e7348f0a8c0a9cfb8bbe
Natithorn <chanwit.u@kkumail.com>
2024-11-25 20:13:32 -0800

    add new project

---
+++ Alex/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Chanwit Unsakul";
+string student_id = "673450187-3";
+// string is Datatype
+// name is variable
+//"..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Ma name is" + name);
+Console.WriteLine("My student id :" + student_id);
